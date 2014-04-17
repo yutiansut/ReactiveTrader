6a914c9739d66c1b978097668aabaa22f4ea1290
Paul Spiteri <paul.spiteri@skylabprime.com>
2014-04-17 20:29:19 +0100

    started moving and porting code to a shared client assembly

---
+++ src/Adaptive.ReactiveTrader.Client.WindowsStoreApp/Bootstrapper.cs
@@ -0,0 +1,41 @@
+﻿using Adaptive.ReactiveTrader.Client.Concurrency;
+using Adaptive.ReactiveTrader.Client.Configuration;
+using Adaptive.ReactiveTrader.Client.Domain;
+using Autofac;
+
+namespace Adaptive.ReactiveTrader.Client
+{
+    public class Bootstrapper
+    {
+        public IContainer Build()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Domain.ReactiveTrader>().As<IReactiveTrader>().SingleInstance();
+            builder.RegisterType<ConfigurationProvider>().As<IConfigurationProvider>();
+            builder.RegisterType<UserProvider>().As<IUserProvider>();
+            builder.RegisterType<ConcurrencyService>().As<IConcurrencyService>();
+
+            //// views
+            //builder.RegisterType<ShellView>();
+            //builder.RegisterType<SpotTilesView>();
+            //builder.RegisterType<SpotTileView>();
+            //builder.RegisterType<BlotterView>();
+
+            //// view models
+            //builder.RegisterType<ShellViewModel>().As<IShellViewModel>().ExternallyOwned();
+            //builder.RegisterType<SpotTilesViewModel>().As<ISpotTilesViewModel>().ExternallyOwned();
+            //builder.RegisterType<SpotTileViewModel>().As<ISpotTileViewModel>().ExternallyOwned();
+            //builder.RegisterType<SpotTileErrorViewModel>().As<ISpotTileErrorViewModel>().ExternallyOwned();
+            //builder.RegisterType<SpotTileConfigViewModel>().As<ISpotTileConfigViewModel>().ExternallyOwned();
+            //builder.RegisterType<SpotTilePricingViewModel>().As<ISpotTilePricingViewModel>().ExternallyOwned();
+            //builder.RegisterType<OneWayPriceViewModel>().As<IOneWayPriceViewModel>().ExternallyOwned();
+            //builder.RegisterType<SpotTileAffirmationViewModel>().As<ISpotTileAffirmationViewModel>().ExternallyOwned();
+            //builder.RegisterType<BlotterViewModel>().As<IBlotterViewModel>().ExternallyOwned();
+            //builder.RegisterType<TradeViewModel>().As<ITradeViewModel>().ExternallyOwned();
+            //builder.RegisterType<ConnectivityStatusViewModel>().As<IConnectivityStatusViewModel>().ExternallyOwned();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ src/Adaptive.ReactiveTrader.Client.WindowsStoreApp/Configuration/UserProvider.cs
@@ -0,0 +1,15 @@
+﻿using Windows.ApplicationModel.Core;
+
+namespace Adaptive.ReactiveTrader.Client.Configuration
+{
+    class UserProvider : IUserProvider
+    {
+        public string Username
+        {
+            get
+            {
+                return "Trader-" + CoreApplication.Id;
+            }
+        }
+    }
+}
