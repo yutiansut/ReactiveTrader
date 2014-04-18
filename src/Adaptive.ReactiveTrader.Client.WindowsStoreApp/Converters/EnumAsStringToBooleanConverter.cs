7bb6e6a71ed296c43e2384a17e10a58ebeae83bd
Paul Spiteri <paul.spiteri@skylabprime.com>
2014-04-18 17:48:28 +0100

    corrected the styling on a lot of the spot tile pricing view

---
+++ src/Adaptive.ReactiveTrader.Client.WindowsStoreApp/Converters/EnumAsStringToBooleanConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace Adaptive.ReactiveTrader.Client.Converters
+{
+    public class EnumAsStringToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Enum)
+            {
+                return value.ToString() == (string)parameter;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
