3cae74e7b67a11cccc8d380413226313d344c656
Paul Spiteri <paul.spiteri@skylabprime.com>
2014-04-19 00:02:39 +0100

    more spot tile styling.

--- src/Adaptive.ReactiveTrader.Client.WindowsStoreApp/Converters/ToStringConverter.cs
+++ src/Adaptive.ReactiveTrader.Client.WindowsStoreApp/Converters/ToStringConverter.cs
@@ -3,15 +3,11 @@ using Windows.UI.Xaml.Data;
 
 namespace Adaptive.ReactiveTrader.Client.Converters
 {
-    public class EnumAsStringToBooleanConverter : IValueConverter
+    public class ToStringConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            if (value is Enum)
-            {
-                return value.ToString() == (string)parameter;
-            }
-            return false;
+            return value != null ? value.ToString() : null;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
