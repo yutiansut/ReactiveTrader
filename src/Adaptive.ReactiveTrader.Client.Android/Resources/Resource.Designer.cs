ecd65f24180f1440b3177e0006d470f30418fd62
Paul Spiteri <paulspiteri@hotmail.com>
2015-01-25 19:46:41 +0000

    axml update

--- src/Adaptive.ReactiveTrader.Client.Android/Resources/Resource.Designer.cs
+++ src/Adaptive.ReactiveTrader.Client.Android/Resources/Resource.Designer.cs
@@ -60,20 +60,23 @@ namespace Adaptive.ReactiveTrader.Client.Android
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050005
+			public const int SpotTileAskButton = 2131034117;
+			
 			// aapt resource value: 0x7f050004
-			public const int SpotTileAskButton = 2131034116;
-			
-			// aapt resource value: 0x7f050003
-			public const int SpotTileBidButton = 2131034115;
-			
-			// aapt resource value: 0x7f050001
-			public const int SpotTileCurrencyPairTextView = 2131034113;
+			public const int SpotTileBidButton = 2131034116;
 			
 			// aapt resource value: 0x7f050002
-			public const int SpotTileUpDownLayout = 2131034114;
+			public const int SpotTileCurrencyPairTextView = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int SpotTileUpDownLayout = 2131034115;
 			
 			// aapt resource value: 0x7f050000
 			public const int SpotTilesRecyclerView = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int linearLayout1 = 2131034113;
 			
 			static Id()
 			{
