d09dd4e1063e37be77a7f10dc8a318ebda3f1613
Tom Gilder <tom@tom.me.uk>
2015-11-26 14:36:20 +0000

    Upgrade projects

---
+++ src/Adaptive.ReactiveTrader.Tests/Fody/FodyHasRunTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Adaptive.ReactiveTrader.Client;
+
+namespace Adaptive.ReactiveTrader.Tests
+{
+    [TestFixture]
+    public class FodyHasRunTests
+    {
+        [Test]
+        public void Fody_Has_Run_On_Client()
+        {
+            var changes = new List<string>();
+            var test = new TestClass();
+            test.PropertyChanged += (sender, e) => changes.Add(e.PropertyName);
+            test.Test = "hello world";
+            Assert.AreEqual(1, changes.Count);
+        }
+    }
+}
+
